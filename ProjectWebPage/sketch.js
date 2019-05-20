e3750ebfe23569c1092619de7e1d5a8bb2996eeb
jshart <jshart@gmail.com>
2019-05-20 16:24:52 +0100

    created a mini rain animation

--- ProjectWebPage/sketch.js
+++ ProjectWebPage/sketch.js
@@ -1,8 +1,33 @@
+let rainX=[];
+let rainY=[];
+let rainSpeed=[];
+
 function setup() {
   createCanvas(500,500);
+
+  let a=0;
+  for (a=0;a<10;a++)
+  {
+    rainX[a]=random(0,width);
+    rainY[a]=0;
+    rainSpeed[a]=random(1,3);
+  }
 }
 
 function draw() {
     background(255);
     ellipse(mouseX,mouseY,10,10);
+
+    let a=0;
+    for (a=0;a<10;a++)
+    {
+      ellipse(rainX[a],rainY[a],5,5);
+      rainY[a]+=rainSpeed[a];
+
+      if (rainY[a]>height)
+      {
+        rainY[a]=0;
+        rainX[a]=random(0,width);
+      }
+    }
 }
