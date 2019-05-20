acb159d85938b9664484ec27c57768cf4084e47a
jshart <jshart@gmail.com>
2019-05-20 16:50:29 +0100

    changed to snow

--- ProjectWebPage/sketch.js
+++ ProjectWebPage/sketch.js
@@ -1,16 +1,25 @@
-let rainX=[];
-let rainY=[];
-let rainSpeed=[];
+let snowX=[];
+let snowY=[];
+let snowSpeed=[];
+
+let maxDrops=20;
+
+let snowLevel=[];
 
 function setup() {
   createCanvas(500,500);
 
   let a=0;
-  for (a=0;a<10;a++)
+  for (a=0;a<maxDrops;a++)
   {
-    rainX[a]=random(0,width);
-    rainY[a]=0;
-    rainSpeed[a]=random(1,3);
+    snowX[a]=floor(random(0,width));
+    snowY[a]=0;
+    snowSpeed[a]=random(1,3);
+  }
+
+  for (a=0;a<width;a++)
+  {
+    snowLevel[a]=0;
   }
 }
 
@@ -19,15 +28,32 @@ function draw() {
     ellipse(mouseX,mouseY,10,10);
 
     let a=0;
-    for (a=0;a<10;a++)
+    for (a=0;a<maxDrops;a++)
     {
-      ellipse(rainX[a],rainY[a],5,5);
-      rainY[a]+=rainSpeed[a];
+      ellipse(snowX[a],snowY[a],5,5);
+      snowY[a]+=snowSpeed[a];
 
-      if (rainY[a]>height)
+      if (snowY[a]>(height-snowLevel[snowX[a]]))
+      //if (snowY[a]>(height))
       {
-        rainY[a]=0;
-        rainX[a]=random(0,width);
+        snowLevel[snowX[a]]+=2;
+
+        if (snowX[a]>0)
+        {
+          snowLevel[snowX[a]-1]++;
+        }
+        if (snowX[a]<width)
+        {
+          snowLevel[snowX[a]+1]++;
+        }
+
+        snowY[a]=0;
+        snowX[a]=floor(random(0,width));
       }
+    }
+
+    for (a=0;a<width;a++)
+    {
+      line(a,height-snowLevel[a],a,height);
     }
 }
